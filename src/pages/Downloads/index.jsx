a5ee319c7a1d78e7696cc536bd16280f9571aa0e
AnierinB <anierin@evolution-x.org>
2025-03-30 14:48:51 -0700

    www: Make device card border evo blue
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/pages/Downloads/index.jsx
+++ src/pages/Downloads/index.jsx
@@ -255,7 +255,7 @@ const Downloads = () => {
                       whileInView={{ opacity: 1, scale: 1 }}
                       key={index}
                     >
-                      <div className="relative flex min-h-full w-[23rem] flex-col justify-between rounded-2xl border border-slate-800 bg-black pb-7 duration-100 ease-in shadow-[0px_0px_38.5px_14px_#0060ff20] hover:scale-105 hover:shadow-[0px_0px_38.5px_14px_#0060ff50]">
+                      <div className="relative flex min-h-full w-[23rem] flex-col justify-between rounded-2xl border border-[#0060ff] bg-black pb-7 duration-100 ease-in shadow-[0px_0px_38.5px_14px_#0060ff20] hover:scale-105 hover:shadow-[0px_0px_38.5px_14px_#0060ff50]">
                         <img
                           className="mx-auto my-4 flex size-56 object-contain"
                           src={`https://github.com/Evolution-X/www_gitres/blob/main/devices/images/${device.codename}.png?raw=true`}
